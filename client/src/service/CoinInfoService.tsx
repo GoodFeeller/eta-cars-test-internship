c987304cc96d5a8ea7cc0855c16cb7db0a574a86
GoodFeeller <maksim.vasilenok@gmail.com>
2023-11-13 05:14:53 +0300

    Deploy

--- client/src/service/CoinInfoService.tsx
+++ client/src/service/CoinInfoService.tsx
@@ -4,14 +4,6 @@ import { IHistory } from '../types/IHistory'
 import trpc from './appRouter'
 
 const CoinInfoService = {
-  getCoin: async function (id: string): Promise<ICoin> {
-    try {
-      const response: ICoin = await trpc.coin.getCoin.query(id)
-      return response
-    } catch (err) {
-      throw new Error('Not found')
-    }
-  },
   getHistory: async function (
     id: string,
     interval: intervalType,
